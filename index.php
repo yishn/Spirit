cc682ba00034a72476c94aa586e120cbf6e1a37d
yishn <shen.yichuan@gmail.com>
2015-01-02 23:19:01 +0100

    Change folder structure

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once('spirit/includes/includes.php');
+require_once('spirit/includes.php');
 
 dispatch();
---
+++ spirit/includes.php
@@ -0,0 +1,14 @@
+<?php
+
+$libDir = dirname(__FILE__) . '/lib/';
+$items = scandir($libDir);
+
+foreach ($items as $item) {
+    if (substr($item, -4) === '.php' && file_exists($libDir . $item)) {
+        require_once($libDir . $item);
+    }
+}
+
+function __autoload($class) {
+    require_once(dirname(__FILE__) . "/classes/{$class}.php");
+}
