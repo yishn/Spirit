b60c594121cc4947f6ededd4369fc7752d9d9dca
yishn <shen.yichuan@gmail.com>
2015-01-02 22:20:37 +0100

    Init commit

---
+++ spirit/lib/mustache.php
@@ -0,0 +1,229 @@
+<?php
+
+/**
+ * Mustache
+ * @package Simplon\Mustache
+ * @author Tino Ehrich (tino@bigpun.me)
+ */
+class Mustache
+{
+    /**
+     * @var array
+     */
+    private static $data;
+
+    /**
+     * @var array
+     */
+    private static $templates = [];
+
+    /**
+     * @param $template
+     * @param array $data
+     * @param array $customParsers
+     *
+     * @return string
+     */
+    public static function render($template, array $data = [], array $customParsers = [])
+    {
+        // cache data
+        self::$data = $data;
+
+        // parse template
+        $template = self::parse($template, $data);
+
+        // run custom parsers
+        $template = self::handleCustomParsers($template, $customParsers);
+
+        // clean left overs and reset data
+        return self::finaliseTemplate($template);
+    }
+
+    /**
+     * @param $pathTemplate
+     * @param array $data
+     * @param array $customParsers
+     * @param string $fileExtension
+     *
+     * @return string
+     * @throws MustacheException
+     */
+    public static function renderByFile($pathTemplate, array $data = [], array $customParsers = [], $fileExtension = 'mustache')
+    {
+        // set filename
+        $fileName = $pathTemplate . '.' . $fileExtension;
+
+        // test cache
+        if (isset(self::$templates[$pathTemplate]) === false)
+        {
+            // make sure the file exists
+            if (file_exists($fileName) === false)
+            {
+                throw new MustacheException('Missing given template file: ' . $fileName);
+            }
+
+            // fetch template
+            $template = file_get_contents($fileName);
+
+            if ($template === false)
+            {
+                throw new MustacheException('Could not load template file: ' . $fileName);
+            }
+
+            // cache template
+            self::$templates[$pathTemplate] = $template;
+        }
+
+        return self::render(self::$templates[$pathTemplate], $data, $customParsers);
+    }
+
+    /**
+     * @param $template
+     * @param array $data
+     *
+     * @return string
+     */
+    private static function parse($template, array $data = [])
+    {
+        foreach ($data as $key => $val)
+        {
+            if (is_array($val) && empty($val) === false)
+            {
+                // find loops
+                preg_match_all('|{{#' . $key . '}}(.*?){{/' . $key . '}}|sm', $template, $foreachPattern);
+
+                // handle loops
+                if (isset($foreachPattern[1][0]))
+                {
+                    foreach ($foreachPattern[1] as $patternId => $patternContext)
+                    {
+                        $loopContent = '';
+
+                        // handle array objects
+                        if (isset($val[0]))
+                        {
+                            foreach ($val as $loopVal)
+                            {
+                                // make simple lists available
+                                if (is_array($loopVal) === false)
+                                {
+                                    $loopVal = ['_' => $loopVal];
+                                }
+
+                                $loopContent .= self::parse($patternContext, $loopVal);
+                            }
+                        }
+
+                        // normal array only
+                        else
+                        {
+                            $loopContent = self::parse($patternContext, $val);
+                        }
+
+                        // replace pattern context
+                        $template = preg_replace(
+                            '|' . preg_quote($foreachPattern[0][$patternId]) . '|s',
+                            $loopContent,
+                            $template,
+                            1
+                        );
+                    }
+                }
+            }
+
+            // ----------------------------------
+
+            elseif (is_bool($val) || empty($val) === true)
+            {
+                // determine true/false
+                $conditionChar = $val === true ? '\#' : '\^';
+
+                // find bools
+                preg_match_all('|{{' . $conditionChar . $key . '}}(.*?){{/' . $key . '}}|s', $template, $boolPattern);
+
+                // handle bools
+                if (isset($boolPattern[1][0]))
+                {
+                    foreach ($boolPattern[1] as $patternId => $patternContext)
+                    {
+                        // parse and replace pattern context
+                        $template = preg_replace(
+                            '|' . preg_quote($boolPattern[0][$patternId]) . '|s',
+                            self::parse($patternContext, self::$data),
+                            $template,
+                            1
+                        );
+                    }
+                }
+            }
+
+            // ----------------------------------
+
+            elseif ($val instanceof \Closure)
+            {
+                // set closure return
+                $template = str_replace('{{' . $key . '}}', $val(), $template);
+            }
+
+            // ----------------------------------
+
+            else
+            {
+                // set var: unescaped
+                $template = str_replace('{{{' . $key . '}}}', $val, $template);
+
+                // set var: escaped
+                $template = str_replace('{{' . $key . '}}', htmlspecialchars($val), $template);
+            }
+        }
+
+        return (string)$template;
+    }
+
+    /**
+     * @param string $template
+     * @param array $parsers
+     *
+     * @return string
+     */
+    private static function handleCustomParsers($template, array $parsers = [])
+    {
+        foreach ($parsers as $parser)
+        {
+            if (isset($parser['pattern']) && isset($parser['callback']))
+            {
+                preg_match_all('|' . $parser['pattern'] . '|', $template, $match);
+
+                if (isset($match[1][0]))
+                {
+                    $template = $parser['callback']($template, $match);
+                }
+            }
+        }
+
+        return (string)$template;
+    }
+
+    /**
+     * @param string $template
+     *
+     * @return string
+     */
+    private static function finaliseTemplate($template)
+    {
+        // remove left over wrappers
+        $template = preg_replace('|{{.*?}}.*?{{/.*?}}\n*|s', '', $template);
+
+        // remove left over variables
+        $template = preg_replace('|{{.*?}}\n*|s', '', $template);
+
+        return (string)$template;
+    }
+}
+
+/**
+ * MustacheException
+ * @package Simplon\Mustache
+ * @author Tino Ehrich (tino@bigpun.me)
+ */
+class MustacheException extends Exception {}
