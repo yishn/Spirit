ab0013cb719b73476e2797dfc0e9417c1d0d6e0f
yishn <shen.yichuan@gmail.com>
2015-01-03 22:59:32 +0100

    Move generateThumbnail to Photo class

--- spirit/classes/Photo.php
+++ spirit/classes/Photo.php
@@ -21,16 +21,30 @@ class Photo extends Model {
         return Dispatcher::link("/photo/{$this->id}/size/large");
     }
 
+    public function getEditLink() {
+        return Dispatcher::link("/spirit/edit/photo/{$this->id}");
+    }
+
     public function getFormattedDescription() {
         $parsedown = new Parsedown();
         return $parsedown->text($this->description);
     }
 
+    public function generateThumbnail($size, $trim) {
+        $filename = $this->filename;
+        $contentDir = Setting::where('key', 'contentDir')->find_one()->value;
+        $path = ABSURL . "/{$contentDir}photos/" . $filename;
+
+        Thumb::render($path, $size, $trim);
+    }
+
     public function as_array() {
         $result = parent::as_array();
+
         $result['permalink'] = $this->getPermalink();
         $result['thumbnailLink'] = $this->getThumbnailLink();
         $result['largeImageLink'] = $this->getLargeImageLink();
+        $result['editLink'] = $this->getEditLink();
         $result['description'] = $this->getFormattedDescription();
 
         return $result;
--- spirit/classes/Spirit.php
+++ spirit/classes/Spirit.php
@@ -4,24 +4,21 @@ class Spirit {
     public static function setUpRoutes() {
         Dispatcher::map('GET', '/', function() { echo "Void."; });
 
-        Dispatcher::map('GET', '/photo/{id:\d*}', function($params) { echo "Photo ID " . $params['id']; });
-        Dispatcher::map('GET', '/photo/{id:\d*}/size/thumb', function($params) {
-            $size = Setting::where('key', 'thumbSize')->find_one()->value;
-            Spirit::generateThumbnail($params['id'], $size, true);
-        });
-        Dispatcher::map('GET', '/photo/{id:\d*}/size/large', function($params) {
-            $size = Setting::where('key', 'photoSize')->find_one()->value;
-            Spirit::generateThumbnail($params['id'], $size, false);
+        Dispatcher::map('GET', '/photo/{id:\d*}/size/{size:thumb|large}', function($params) {
+            $photo = Photo::find_one($params['id']);
+            if (!$photo) Dispatcher::error(404);
+            
+            $size = Setting::where('key', $params['size'] == 'thumb' ? 'thumbSize' : 'largeImageSize')
+                ->find_one()
+                ->value;
+            $photo->generateThumbnail($size, true);
         });
 
         Dispatcher::map('GET', '/spirit/login', function() { 
             print self::renderLogin();
         });
 
-        Dispatcher::map('GET', '/spirit', function() {
-            Dispatcher::redirect('/spirit/photos');
-        });
-
+        Dispatcher::map('GET', '/spirit', function() { Dispatcher::redirect('/spirit/photos'); });
         Dispatcher::map('GET', '/spirit/{page:photos|albums|users|settings}', function($params) {
             $admin = new SpiritAdmin();
             print $admin->renderAdmin($params['page']);
@@ -34,19 +31,5 @@ class Spirit {
         ];
         
         return Mustache::renderByFile('spirit/views/login', $context);
-    }
-
-    public static function generateThumbnail($photoId, $size, $trim) {
-        $photo = Photo::find_one($photoId);
-        if (!$photo) {
-            Dispatcher::error(404);
-            return;
-        }
-
-        $filename = $photo->filename;
-        $contentDir = Setting::where('key', 'contentDir')->find_one()->value;
-        $path = ABSURL . "/{$contentDir}photos/" . $filename;
-
-        Thumb::render($path, $size, $trim);
     }
 }
