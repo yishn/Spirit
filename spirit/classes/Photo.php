30a3a5c66bcdfe79822fa290b5882986f767efc4
yishn <shen.yichuan@gmail.com>
2015-01-03 22:16:09 +0100

    Add additional functions to Photo

--- spirit/classes/Photo.php
+++ spirit/classes/Photo.php
@@ -8,4 +8,31 @@ class Photo extends Model {
     public function user() {
         return $this->belongs_to('User');
     }
+
+    public function getPermalink() {
+        return Dispatcher::link("/photo/{$this->id}");
+    }
+
+    public function getThumbnailLink() {
+        return Dispatcher::link("/photo/{$this->id}/size/thumb");
+    }
+
+    public function getLargeImageLink() {
+        return Dispatcher::link("/photo/{$this->id}/size/large");
+    }
+
+    public function getFormattedDescription() {
+        $parsedown = new Parsedown();
+        return $parsedown->text($this->description);
+    }
+
+    public function as_array() {
+        $result = parent::as_array();
+        $result['permalink'] = $this->getPermalink();
+        $result['thumbnailLink'] = $this->getThumbnailLink();
+        $result['largeImageLink'] = $this->getLargeImageLink();
+        $result['description'] = $this->getFormattedDescription();
+
+        return $result;
+    }
 }
