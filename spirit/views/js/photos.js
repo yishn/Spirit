6237d8518df1016eb26b9e92eb446b8e49978661
yishn <shen.yichuan@gmail.com>
2015-01-06 19:32:08 +0100

    Center photos

--- spirit/views/js/photos.js
+++ spirit/views/js/photos.js
@@ -4,15 +4,20 @@ document.addEvent('domready', function() {
  * Photostream selection
  */
 
-$$('#toolbox li.edit, #toolbox li.delete').setStyle('visibility', 'hidden');
+$$('#toolbox li.edit, #toolbox li.delete').setStyle(
+    'display', $$('.photostream .selected').length != 0 ? 'block' : 'none'
+);
+
 $$('.photostream li input').removeEvents('change').addEvent('change', function() {
     if (this.checked) this.getParent().addClass('selected');
     else this.getParent().removeClass('selected');
-    $$('#toolbox li.edit, #toolbox li.delete').setStyle('visibility', this.checked ? 'visible' : 'hidden');
+    $$('#toolbox li.edit, #toolbox li.delete').setStyle(
+        'display', $$('.photostream .selected').length != 0 ? 'block' : 'none'
+    );
 });
 
 /**
- * Infinite scrolling
+ * Load more button
  */
 
 $$('#pagination .previous').getParent().setStyle('display', 'none');
@@ -27,7 +32,7 @@ $$('#pagination .next').set('text', 'Load more photos').removeEvents('click').ad
             $$('.photostream').adopt(ell.filter('.photostream')[0].getElements('li'));
             $('pagination').adopt(ell.filter('#pagination')[0].getElements('li'));
             document.fireEvent('domready');
-            next.dispose();
+            next.getParent().dispose();
         }
     }).get();
 });
