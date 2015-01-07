ef3874e457f96c1e298ee5a4cbd1fcaf3a4f1372
yishn <shen.yichuan@gmail.com>
2015-01-07 01:50:55 +0100

    Add autoresize ace

--- spirit/views/js/markdown-editor.js
+++ spirit/views/js/markdown-editor.js
@@ -15,7 +15,10 @@ editor.setDisplayIndentGuides(false);
 editor.setHighlightSelectedWord(false);
 editor.renderer.setPadding(0);
 editor.renderer.setShowGutter(false);
+editor.setAutoScrollEditorIntoView(true);
+editor.setOption('minLines', 5);
+editor.setOption('maxLines', 100);
 
-$('ace-tm').set('text', $('ace-tm').get('text') + ' .ace_line * { color: black !important; } .ace_heading, .ace_strong, .ace_function { font-weight: bold; } .ace_underline, .ace_url { text-decoration: underline; } .ace_emphasis { font-style: italic; } .ace_blockquote { opacity: .7; }');
+$('ace-tm').set('text', $('ace-tm').get('text') + ' .ace_line * { color: black !important; } .ace_heading, .ace_strong, .ace_function { font-weight: bold; } .ace_underline, .ace_url { text-decoration: underline; } .ace_emphasis { font-style: italic; } .ace_blockquote { opacity: .7; } .ace_hidden-cursors { opacity: 0; }');
 
 });
