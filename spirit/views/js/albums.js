60ef7fc708ecf58b18932fc16c874b11bff11b9f
yishn <shen.yichuan@gmail.com>
2015-01-10 02:35:55 +0100

    Add large dialog style

--- spirit/views/js/albums.js
+++ spirit/views/js/albums.js
@@ -8,11 +8,16 @@ $$('.albumstream > li').each(function(li) {
     li.setStyle('background-position', 'center');
 });
 
-$$('#toolbox .add a').addEvent('click', function(e) {
+$$('#toolbox .add a, .albumstream ul li.edit a').addEvent('click', function(e) {
     e.preventDefault();
+    var link = this;
+
     $('dialog').load(this.get('href'));
-    $('dialog').addEvent('shown', function() {
+    $('dialog').removeEvents('shown').addEvent('shown', function() {
         $$('#dialog main input')[0].focus();
+
+        if (link.getParent().hasClass('add'))
+            $$('#dialog main button').set('text', 'Create');
     });
 });
 
