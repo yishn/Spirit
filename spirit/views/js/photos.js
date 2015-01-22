00f47071f81fda55cb40b810d267420d95c33a55
yishn <shen.yichuan@gmail.com>
2015-01-22 22:19:25 +0100

    Add Dropzone.js

--- spirit/views/js/photos.js
+++ spirit/views/js/photos.js
@@ -4,10 +4,33 @@ document.addEvent('domready', function() {
  * Photostream selection
  */
 
+// Configure Dropzone and upload form
+
 $$('#toolbox .upload a').addEvent('click', function(event) {
     event.preventDefault();
     $('dialog').load(this.get('href'));
+    $('dialog').removeEvents('shown').addEvent('shown', function() {
+        var form = $$('#dialog form')[0];
+        form.grab(new Element('div', { class: 'uploadqueue' }), 'bottom');
+
+        form.store('dropzone', new Dropzone(form, {
+            uploadMultiple: true,
+            addRemoveLinks: true,
+            acceptedFiles: 'image/*',
+            autoProcessQueue: false,
+            previewsContainer: form.getElement('.uploadqueue'),
+            thumbnailWidth: 100,
+            thumbnailHeight: 100
+        }));
+
+        $$('#dialog button[type="submit"]').addEvent('click', function(e) {
+            e.preventDefault();
+            form.retrieve('dropzone').processQueue();
+        });
+    });
 });
+
+// Photo selection
 
 $$('#toolbox li.edit, #toolbox li.delete').setStyle(
     'display', $$('.photostream .selected').length != 0 ? 'block' : 'none'
@@ -20,6 +43,8 @@ $$('.photostream li input').removeEvents('change').addEvent('change', function()
         'display', $$('.photostream .selected').length != 0 ? 'block' : 'none'
     );
 });
+
+// Bulk edit/delete
 
 $$('#toolbox .edit a, #toolbox .delete a').addEvent('click', function(e) {
     e.preventDefault();
