9744a80d11ba614d9151f963ac9d3be08c2083a7
yishn <shen.yichuan@gmail.com>
2015-02-03 15:18:33 +0100

    find_one user

--- spirit/classes/Admin.php
+++ spirit/classes/Admin.php
@@ -7,7 +7,7 @@ class Admin {
     }
 
     public function isLoggedIn() {
-        $this->user = User::where('email', 'shen.yichuan@gmail.com')->find_one();
+        $this->user = User::find_one(3);
         return true;
     }
 
