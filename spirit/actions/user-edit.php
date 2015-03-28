59081778ccdd6b09b5233cc43347d47613df4471
yishn <shen.yichuan@gmail.com>
2015-03-28 15:22:26 +0100

    Add user-edit action

---
+++ spirit/actions/user-edit.php
@@ -0,0 +1,22 @@
+<?php
+
+$user = User::find_one($params['id']);
+if (!$user) $user = User::create();
+
+if ($_POST['name'] == '' || $_POST['email'] == '')
+    Route::redirect('/spirit/users/' . $user->id);
+
+$user->set([
+    'name' => $_POST['name'],
+    'email' => $_POST['email']
+]);
+
+if ($_POST['password'] != '') {
+    if ($_POST['verifypassword'] != $_POST['password'])
+        Route::redirect('/spirit/users/' . $user->id)
+
+    $user->generateHash($_POST['password']);
+}
+
+$user->save();
+Route::redirect('/spirit/users');
