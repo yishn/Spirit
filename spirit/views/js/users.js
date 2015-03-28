f05c1e082225e81a63b5c0b8ab3808ff5412498a
yishn <shen.yichuan@gmail.com>
2015-03-29 00:04:36 +0100

    Confirmation for deleting user

--- spirit/views/js/users.js
+++ spirit/views/js/users.js
@@ -6,6 +6,11 @@ $$('#toolbox .add a, .userstream li a').addEvent('click', function(event) {
     $('dialog').load(this.get('href'));
     $('dialog').removeEvents('shown').addEvent('shown', function() {
         $$('#dialog main input')[0].focus();
+        $$('#dialog .delete').addEvent('click', function(event) {
+            if (!window.confirm("Are you sure you want to delete this user? This action cannot be undone.")) {
+                event.preventDefault();
+            }
+        });
     });
 });
 
