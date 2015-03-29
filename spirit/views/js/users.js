33431dde7fdb2c754b60a64bbffd72bc38779304
yishn <shen.yichuan@gmail.com>
2015-03-29 13:30:37 +0200

    Give warning when passwords don't coincide

--- spirit/views/js/users.js
+++ spirit/views/js/users.js
@@ -11,6 +11,16 @@ $$('#toolbox .add a, .userstream li a').addEvent('click', function(event) {
                 event.preventDefault();
             }
         });
+        $$('#dialog button[type="submit"]').addEvent('click', function(event) {
+            if ($$('#dialog input[type="password"]')[0].value == '' && $$('#dialog input[type="password"]')[1].value == '')
+                return;
+            if ($$('#dialog input[type="password"]')[0].value == $$('#dialog input[type="password"]')[1].value) 
+                return;
+
+            alert('The passwords do not coincide!');
+            $$('#dialog input[type="password"]').set('value', '')[0].focus();
+            event.preventDefault();
+        });
     });
 });
 
