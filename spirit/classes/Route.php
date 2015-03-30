cca0e2ee47592a7aae9ce33228c9de3a99ed8213
yishn <shen.yichuan@gmail.com>
2015-03-30 16:53:49 +0200

    Autofocus login form

--- spirit/classes/Route.php
+++ spirit/classes/Route.php
@@ -8,7 +8,7 @@ class Route extends Dispatcher {
     }
 
     private static function mapTheme() {
-        parent::map('GET', '/', function() { phpinfo(); });
+        parent::map('GET', '/', function() { echo "Void."; });
         parent::map(401, function() { echo "Error 401: Unauthorized"; });
         parent::map(404, function() { echo "Error 404: Not Found"; });
 
