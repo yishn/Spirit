f743756a0b138382d0a37234e106b33afec297f9
yishn <shen.yichuan@gmail.com>
2015-03-30 13:42:24 +0200

    Fewer album queries

--- spirit/classes/Album.php
+++ spirit/classes/Album.php
@@ -4,7 +4,8 @@ class Album extends Model {
     public function photos($overrideDescOrder = false) {
         $query = $this->has_many_through('Photo');
 
-        if ($this->chronological == 1 && !$overrideDescOrder) return $query->order_by_asc('date')->order_by_asc('id');
+        if ($this->chronological == 1 && !$overrideDescOrder)
+            return $query->order_by_asc('date')->order_by_asc('id');
         return $query->order_by_desc('date')->order_by_desc('id');
     }
 
