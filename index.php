85d77cd473c6c909f5356ed45901270865b65cc8
Yichuan Shen <shen.yichuan@gmail.com>
2016-03-17 19:41:55 +0100

    Add spirit_journals()

--- index.php
+++ index.php
@@ -2,7 +2,15 @@
 
 require_once('config.php');
 
+function serve_json() {
+    header('Content-Type: application/json');
+}
+
 spirit_route('GET', '/', function() { echo 'Hello World!'; });
+
+spirit_route('GET', '/json/journals', [serve_json, function() {
+    echo json_encode(spirit_journals());
+}]);
 
 spirit_route('*', '*', function() { render('view/error.phtml'); });
 
--- lib/spirit.php
+++ lib/spirit.php
@@ -1,12 +1,53 @@
 <?php
 
+class SpiritParsedown extends Parsedown {
+    protected function blockHeader($line) {
+        $block = parent::blockHeader($line);
+
+        if (!$block) return;
+        if ($block['element']['name'] != 'h1') return $block;
+
+        $text = $block['element']['text'];
+        $this->title = $text;
+
+        return $block;
+    }
+}
+
 function spirit_route($method, $paths, $funcs) {
     if (!is_array($paths)) $paths = [$paths];
 
     $paths = array_map(function($x) {
-        if ($x != '*') return trim(BASE_PATH, '/') . rtrim($x, '/');
+        if ($x !== '*') return trim(BASE_PATH, '/') . rtrim($x, '/');
         return $x;
     }, $paths);
 
     return route($method, $paths, $funcs);
+}
+
+function spirit_journals() {
+    $paths = glob('content/*', GLOB_ONLYDIR);
+    $result = [];
+
+    foreach ($paths as $path) {
+        if ($path[0] === '_') continue;
+
+        $mds = glob($path . '/*.md');
+        if (count($mds) === 0) continue;
+
+        $md = file_get_contents($mds[0]);
+        $parsedown = new SpiritParsedown();
+        $parsedown->text($md);
+        if (!isset($parsedown->title)) continue;
+
+        $id = preg_replace('/^\d+-/', '', basename($path));
+
+        $result[] = [
+            'id' => $id,
+            'path' => $path,
+            'name' => $parsedown->title
+        ];
+    }
+
+    return $result;
 }
