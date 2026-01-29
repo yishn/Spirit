447f35156b1d656c9737dd277a7dc2820888aa26
Yichuan Shen <shen.yichuan@gmail.com>
2026-01-29 02:46:01 +0100

    Fix palette detection issues

--- src/palette.ts
+++ src/palette.ts
@@ -24,46 +24,48 @@ function getPaletteFromImg(img: HTMLImageElement): Color[] | null {
   return palette;
 }
 
-function updatePalette(img: HTMLImageElement) {
-  const palette = getPaletteFromImg(img);
-  if (palette == null) return;
-
-  document.body.style.setProperty(
-    "--main-image",
-    `url("${img.getAttribute("src")}")`,
-  );
-  document.body.style.setProperty(
-    "--background-color",
-    `rgb(${palette[0].map((x) => x * 0.8).join(", ")})`,
-  );
-  document.body.style.setProperty(
-    "--color",
-    `rgb(${palette.at(-1)!.join(", ")})`,
-  );
-  document.body.style.setProperty(
-    "--link-color",
-    `rgb(${palette.at(-2)!.join(", ")})`,
-  );
-}
-
 let timeout: ReturnType<typeof setTimeout> | undefined;
+
+function updatePalette(img: HTMLImageElement) {
+  clearTimeout(timeout);
+
+  timeout = setTimeout(() => {
+    const palette = getPaletteFromImg(img);
+    if (palette == null) return;
+
+    document.body.style.setProperty(
+      "--main-image",
+      `url("${img.getAttribute("src")}")`,
+    );
+    document.body.style.setProperty(
+      "--background-color",
+      `rgb(${palette[0].map((x) => x * 0.8).join(", ")})`,
+    );
+    document.body.style.setProperty(
+      "--color",
+      `rgb(${palette.at(-1)!.join(", ")})`,
+    );
+    document.body.style.setProperty(
+      "--link-color",
+      `rgb(${palette.at(-2)!.join(", ")})`,
+    );
+
+    timeout = undefined;
+  }, 500);
+}
 
 export function initPalette() {
   const observer = new IntersectionObserver(
     (entries) => {
-      clearTimeout(timeout);
+      const el = entries.find(
+        (entry) =>
+          entry.target instanceof HTMLImageElement &&
+          entry.intersectionRatio >= 0.6,
+      )?.target as HTMLImageElement | undefined;
 
-      timeout = setTimeout(() => {
-        const el = entries.find(
-          (entry) => entry.intersectionRatio > 0.5,
-        )?.target;
-
-        if (el != null && el instanceof HTMLImageElement) {
-          updatePalette(el);
-        }
-
-        timeout = undefined;
-      }, 500);
+      if (el != null) {
+        updatePalette(el);
+      }
     },
     { threshold: 0.6 },
   );
